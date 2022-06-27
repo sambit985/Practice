f0421eb4e8475cdd842b7f471e35a157661fd238
sambit985 <kumarsambit678@gmail.com>
2022-06-27 13:55:51 +0530

    Updated file

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent.js';
 import './App.css';
+import { DISHES } from './shared/dishes';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
   render() {
     return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
-         <NavbarBrand href='/'>Restorante confusion </NavbarBrand>
+         <NavbarBrand href='/'>Resturant Items </NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
     );
   }
